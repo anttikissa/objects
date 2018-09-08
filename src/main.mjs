2a62b2e044f99ce37ba316ab8f68875303ec3114
Antti Kissaniemi <antti@resq-club.com>
2018-09-08 13:55:13 +0300

    Add util

--- src/main.mjs
+++ src/main.mjs
@@ -1,7 +1,14 @@
 import { log } from './log'
+import { sleep } from './util'
 
-log('Hello!')
-
-for (let i = 0; i < 10; i++) {
-	log('foo', i)
+async function main() {
+	for (let i = 0; i < 5; i++) {
+		await sleep(300)
+		log('Slept 300 ms')
+	}
+	return 'main returned'
 }
+
+main()
+	.then(result => log(result))
+	.catch(err => log('err', err))
---
+++ src/util.mjs
@@ -0,0 +1,4 @@
+export let sleep = ms =>
+	new Promise(resolve => {
+		setTimeout(resolve, ms)
+	})
